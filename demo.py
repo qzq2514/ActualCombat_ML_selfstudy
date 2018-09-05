e11d2f07a6cf858bb261452608b9e7b194b07d36
qzq2514Mac <qzq2514@outlook.com>
2018-09-05 13:58:38 +0800

    电影推荐算法-《ML》课程推荐算法笔记+数据均值化

---
+++ demo.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+# arr=np.random.randint(1,10,[4,3])
+#
+# print(arr)
+#
+# print(np.mean(arr[1,[1,2]]))
+arr=np.zeros((3,2))
+arr[0][0]=1
+arr[1]=2
+arr[2]=3
+print(arr)
+print("---------------")
+print(2-np.NaN)
+
+
--- 电影推荐算法/main.py
+++ 电影推荐算法/main.py
@@ -44,6 +44,9 @@ movieNo=movies_df["movieRow"].max()+1  #原先的movieId不是连续的，所以
                                        #这时再进行how="inner"的合并，会导致最后合并的表中"movieRow"不会包括所有的电影
                                        #而且这里movieRow是从0-9124
 
+                                       #公众号上直接用ratings_df["movieRow"].max()+1，我仔细想了下也有道理，如果后面的电影都没有被评论，那么就没必要在
+                                       #评分表和记录表中特地为他们预留空间，反正以后进行电影推荐的时候也不会将没有被评分过的电影推荐给用户
+                                       #实际上，movieRow为9123,9124都没有被任何用户评论
 rating=np.zeros((movieNo,userNo))    #表示评分记录表，rating[i,j]表示电影i被用户j评论了额
 flag=0
 ratings_df_len=len(ratings_df)   #求表的行数，也可以用np.shape(ratings_df)[0]
@@ -59,4 +62,32 @@ for ind,row in ratings_df.iterrows():  #遍历每一行,ind是整数(从0开始)
 record=rating>0      #建立记录表，有评分的置为1,保证最后记录表中只有0,1两种值
 record=np.array(record,dtype=np.int)   #bool矩阵转为int型
 # print(record[2,30])    --False
-# print(record)
+print(rating.shape)
+
+
+
+#将评分减去平均值，是为了防止有用户没有对任何电影评论或者有电影没有被任何用户评论
+#这样的话，最后会导致该用户的喜好特征或者该电影的类型成分都是0向量，最后进行电影推荐预测时得到也都是0向量，导致一种死循环
+#而先减去平均值，最后即便得到0向量，再加上平均值后也不会产生0向量的死循环(详见"算法笔记/7.jpg")
+def normalizeRating(_rating,_record):
+    m,n=_rating.shape     #m,n分别表示电影数和用户数
+    _rating_mean=np.zeros((m,1))    #每部电影的平均评分
+    _rating_meaned=np.zeros((m,n))    #
+
+    for i in range(m):        #遍历每部电影(电影下标从0开始)
+        idx=record[i,:]!=0       #找到第i部电影被评分过的用户下标
+        _rating_mean[i][0]=np.mean(_rating[i,idx])   #求第i电影所获得的平均评分
+        _rating_meaned[i,idx]-=_rating_mean[i][0]     #某部电影的所有评分减去该电影的平均分
+    return _rating_mean,_rating_meaned   #这里np.mean求出来是一个实数，可以直接赋值给大小是[m,1]的_rating_mean[i]([1,]的矩阵)
+                                         #也可以直接赋值给_rating_mean[i][0]（是个实数，按理说应该使用这种赋值，但是python允许把实数赋值给[1,]大小的矩阵）
+                                         #如果这里_rating_mean是[m,2],再进行"_rating_mean[i]=kk"的赋值，那么就会采用广播的形式，将_rating_mean第i行全部赋值为kk
+
+rating_mean,rating_meaned=normalizeRating(rating,record)
+# print(rating_meaned)
+
+rating_meaned=np.nan_to_num(rating_meaned)   #如果有较多的电影没有被评分，那么第i个idx就是空集合，则再进行np.mean还是得到空值
+rating_mean = np.nan_to_num(rating_mean)  #这样就会导致最后rating_mean和rating_meaned很多都是空值，使用nan_to_num将空值变为0
+
+
+
+
